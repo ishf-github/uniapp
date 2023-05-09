6cf9a1d0b62dedd9d44e59a4de85fbb24fc856c2
Ismail Fisher <m.ismail.fisher@gmail.com>
2023-05-09 23:47:28 +0100

    Updated region to eu-west-1

--- bakery-app/src/pages/api/aws.js
+++ bakery-app/src/pages/api/aws.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
   accessKeyId: 'AKIA5CU5LL6CWATFWNRX',
   secretAccessKey: 'mKXLdtlIZZBQptdCe3XAsKBLaTs5ZdNEmNYkXPuC',
-  region: 'YOUR_REGION',
+  region: 'eu-west-1',
 });
 
 export default AWS;
